e871569ba87c9052997ec4538db71c905d968fee
“Ygor <igorgarofalo@gmail.com>
2023-10-10 02:10:33 +0200

    added analog clock

--- script/script.js
+++ script/script.js
@@ -14,3 +14,22 @@ const time = function () {
 };
 
 setInterval(time, 1000);
+
+const movebars = function () {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  let rotationAngleMInutes = minutes * 6.083 + 270;
+  let rotationAngleHours = hours * 30.416 + 91.25;
+  let rotationAngleSeconds = seconds * 6.083 + 91.25;
+
+  let hoursbar = document.querySelector(".hoursBar");
+  let minutesbar = document.querySelector(".minutesBar");
+  let secondsBar = document.querySelector(".secondsBar");
+  minutesbar.style.transform = `rotate(${rotationAngleMInutes}deg)`;
+  hoursbar.style.transform = `rotate(${rotationAngleHours}deg)`;
+  secondsBar.style.transform = `rotate(${rotationAngleSeconds}deg)`;
+};
+
+setInterval(movebars, 1000);
